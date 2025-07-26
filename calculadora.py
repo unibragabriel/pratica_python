092af7cde5b60d12352898e5e9441bce6646d2eb
bielarnaud <gabriel.fragoso@grupounibra.com>
2025-07-26 13:05:33 -0300

    site

--- calculadora.py
+++
@@ -1,4 +0,0 @@
-from func import somar, subtrair, dividir, multiplicar
-
-print(somar(5,6))
-print(subtrair(4,2))
--- somando.py
+++
@@ -1,5 +0,0 @@
-from func import somar
-
-num1= int(input('Digite um numero: '))
-num2 = int(input('Digite outro número: '))
-print(f'A soma é {num1 +num2}')
